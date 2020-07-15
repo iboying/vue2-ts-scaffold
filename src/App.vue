3f62599e64e593a7ec78504636ff0183682641c6
iboying <weboying@gmail.com>
2020-07-15 15:20:43 +0800

    feat: init project, add vscode config

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="stylus">
+#app
+  margin-top 60px
+  color #2c3e50
+  text-align center
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+</style>
